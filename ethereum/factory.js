ca7d7a5496f598412bd763f7792a5104573381b1
chaithanya <54025784+chaithanyasubramanyam@users.noreply.github.com>
2020-05-26 14:33:02 +0530

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,6 +2,6 @@ import web3 from './web3';
 
 import Factory from './build/Factory.json';
 
-const instance = new web3.eth.Contract(JSON.parse(Factory.interface),'0x39Ed52B080Dc47dac0C54b847173F5382B45ADEF');
+const instance = new web3.eth.Contract(JSON.parse(Factory.interface),'ADD DEPLOYED FACTORY HERE');
 
 export default instance;
